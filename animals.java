7e73f1a7ca142b55b79257a9014d7f3dd0ed6a52
Filamer Doronio <filamerdoronio@gmail.com>
2023-02-14 10:12:08 -0800

    some simple java stuff

---
+++ animals.java
@@ -0,0 +1,9 @@
+// public class Animals {
+//     public static void main(String[] args) {
+//         Cat myCat = new Cat();
+//         myCat.name = "Cheese";
+//         myCat.age = 1;
+//         Cat myCat2 = new Cat();
+//         System.out.println(myCat.name);
+//     }
+// }
---
+++ primitiveTypes.java
@@ -0,0 +1,18 @@
+class LearnJava {
+    public static void main(String[] args) {
+        // int a = 5;
+        // char b = 'b';
+        // long c = 400;
+        // double d = 3.2;
+        // String name = "Bob";
+        // System.out.println(name.toUpperCase());
+        // Cat myCat = new Cat();
+        // myCat.name = "Cheese";
+        // myCat.age = 1;
+        // Cat myCat2 = new Cat();
+        // System.out.println(myCat.name);
+        // Cat.dingDong();
+        Dog myDog1 = new Dog("Cheese", 1);
+        System.out.println(myDog1.name + " is " + myDog1.age + " years old");
+    }
+}
